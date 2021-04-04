14a1e3918f2ec9ab5e500254668cc2a056878223
Fernando <fernandotomaz@copin.ufcg.edu.br>
2021-04-04 13:02:23 -0300

    adicionei refactoring move function

---
+++ auto/anonFn.js
@@ -0,0 +1,10 @@
+'use strict';
+function anonFn(fn) {
+  // For anonymous functions, showing at the very least the function signature can help in
+  // debugging.
+  var args = extractArgs(fn);
+  if (args) {
+    return 'function(' + (args[1] || '').replace(/[\s\r\n]+/, ' ') + ')';
+  }
+  return 'fn';
+}
